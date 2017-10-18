400456c4c86f31d663d6c23135359bcce90a278f
TTMARSH <ohshuttle@163.com>
2017-10-18 18:29:08 +0800

    second commit

---
+++ app/src/main/java/com/coolweather/android/gson/Forecast.java
@@ -0,0 +1,31 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by PC on 2017/10/18.
+ */
+
+public class Forecast {
+
+    public String date;
+
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class Temperature{
+        public String max;
+        public String min;
+    }
+
+    public class More{
+        @SerializedName("txt_id")
+        public String info;
+    }
+
+
+
+}
